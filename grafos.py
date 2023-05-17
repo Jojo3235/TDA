1fccb7190f80c87543d8d2a5952a196826b833f8
Jojo3235 <joralmand@gmail.com>
2023-05-17 15:31:12 +0200

    cosas bonitas con grafos

---
+++ grafos.py
@@ -0,0 +1,224 @@
+class nodoArista(object):
+    def __init__(self, info, destino):
+        self.info = info
+        self.destino = destino
+        self.sig = None
+
+class nodoVertice(object):
+    def __init__(self, info):
+        self.info = info
+        self.sig = None
+        self.visitado = False
+        self.adyacentes = Arista()
+
+class Grafo(object):
+    def __init__(self, dirigido=True):
+        self.inicio = None
+        self.dirigido = dirigido
+        self.tamanio = 0
+
+class Arista(object):
+    def __init__(self):
+        self.inicio = None
+        self.tamanio = 0
+
+def insertar_vertice(grafo, dato):
+    nodo = nodoVertice(dato)
+    if grafo.inicio is None or grafo.inicio.info > dato:
+        nodo.sig = grafo.inicio
+        grafo.inicio = nodo
+    else:
+        ant = grafo.inicio
+        act = grafo.inicio.sig  
+        while act is not None and act.info < nodo.info:
+            ant = act
+            act = act.sig
+        nodo.sig = act
+        ant.sig = nodo
+    grafo.tamanio += 1
+
+def insertar_arista(grafo, dato, origen, destino):
+    agregar_arista(origen.adyacentes, dato, destino.info)
+    if not grafo.dirigido:
+        agregar_arista(destino.adyacentes, dato, origen.info)
+
+def agregar_arista(origen, dato, destino):
+    nodo = nodoArista(dato, destino)
+    if origen.inicio is None or origen.inicio.destino > destino:
+        nodo.sig = origen.inicio
+        origen.inicio = nodo
+    else:
+        ant = origen.inicio
+        act = origen.inicio.sig
+        while act is not None and act.destino < nodo.destino:
+            ant = act
+            act = act.sig
+        nodo.sig = act
+        ant.sig = nodo
+    origen.tamanio += 1
+
+def eliminar_vertice(grafo, clave):
+    x = None
+    if grafo.inicio.info == clave:
+        x = grafo.inicio.info
+        grafo.inicio = grafo.inicio.sig
+        grafo.tamanio -= 1
+    else:
+        ant = grafo.inicio
+        act = grafo.inicio.sig
+        while act is not None and act.info != clave:
+            ant = act
+            act = act.sig
+        if act is not None:
+            x = act.info
+            ant.sig = act.sig
+            grafo.tamanio -= 1
+    if x is not None:
+        aux = grafo.inicio
+        while aux is not None:
+            if aux.adyacentes.inicio is not None:
+                eliminar_arista(aux.adyacentes, x)
+            aux = aux.sig
+    return x
+
+def eliminar_arista(vertice, destino):
+    x = None
+    if vertice.inicio.destino == destino:
+        x = vertice.inicio.info
+        vertice.inicio = vertice.inicio.sig
+        vertice.tamanio -= 1
+    else: 
+        ant = vertice.inicio
+        act = vertice.inicio.sig
+        while act is not None and act.destino != destino:
+            ant = act
+            act = act.sig
+        if act is not None:
+            x = act.info
+            ant.sig = act.sig
+            vertice.tamanio -= 1
+    return x
+
+def buscar_vertice(grafo, buscado):
+    aux = grafo.inicio
+    while aux is not None and aux.info != buscado:
+        aux = aux.sig
+    return aux
+
+def buscar_arista(vertice, buscado):
+    aux = vertice.adyacentes.inicio
+    while aux is not None and aux.destino != buscado:
+        aux = aux.sig
+    return aux
+
+def tamanio(grafo):
+    return grafo.tamanio
+
+def grafo_vacio(grafo):
+    return grafo.inicio is None 
+
+def existe_paso(grafo, origen, destino):
+    resultado = False
+    if not origen.visitado:
+        origen.visitado = True
+        vadyacentes = origen.adyacentes.inicio
+        while vadyacentes is not None and not resultado:
+            if vadyacentes.info == destino.info:
+                return True
+            elif not vadyacentes.visitado:
+                resultado = existe_paso(grafo, vadyacentes, destino)
+            vadyacentes = vadyacentes.sig
+    return resultado
+
+def adyacentes(vertice):
+    aux = vertice.adyacentes.inicio
+    while aux is not None:
+        print(aux.destino, aux.info)
+        aux = aux.sig
+
+def es_adyacente(vertice, destino):
+    resultado = False
+    aux = vertice.adyacentes.inicio
+    while aux is not None and not resultado:
+        if aux.destino == resultado:
+            resultado = True
+        aux = aux.sig
+    return resultado
+
+def marcar_no_visitado(grafo):
+    aux = grafo.inicio
+    while aux is not None:
+        aux.visitado = False
+        aux = aux.sig
+
+def barrido_vertices(grafo):
+    aux = grafo.inicio
+    while aux is not None:
+        print(aux.info)
+        aux = aux.sig
+
+def barrido_profundidad(grafo, vertice):
+    while vertice is not None:
+        if not vertice.visitado:
+            vertice.visitado = True
+            print(vertice.info)
+            adyacentes = vertice.adyacentes.inicio
+            while adyacentes is not None:
+                adyacente = buscar_vertice(grafo, adyacentes.destino)
+                if not adyacente.visitado:
+                    barrido_profundidad(grafo, adyacente)
+                adyacentes = adyacentes.sig
+        vertice = vertice.sig
+
+from colas import Cola
+def barrido_amplitud(grafo, vertice):
+    cola = Cola()
+    while vertice is not None:
+        if not vertice.visitado:
+            vertice.visitado = True
+            Cola.arrive(cola, vertice)
+            while not Cola.cola_vacia(cola):
+                nodo = Cola.atencion(cola)
+                print(nodo.info)
+                adyacentes = nodo.adyacentes.inicio
+                while adyacentes is not None:
+                    adyacente = buscar_vertice(grafo, adyacentes.destino)
+                    if not adyacente.visitado:
+                        adyacente.visitado = True
+                        Cola.arrive(cola, adyacente)
+                    adyacentes = adyacentes.sig
+        vertice = vertice.sig
+
+def mostrar_aristas(grafo):
+    aux = grafo.inicio
+    while aux is not None:
+        print(aux.info)
+        adyacentes = aux.adyacentes.inicio
+        while adyacentes is not None:
+            print(adyacentes.destino, adyacentes.info)
+            adyacentes = adyacentes.sig
+        aux = aux.sig
+
+grafo = Grafo(True)
+insertar_vertice(grafo, 1)
+insertar_vertice(grafo, 2)
+insertar_vertice(grafo, 3)
+insertar_vertice(grafo, 4)
+insertar_vertice(grafo, 5)
+insertar_vertice(grafo, 6)
+
+vertice1 = buscar_vertice(grafo, 1)
+vertice2 = buscar_vertice(grafo, 2)
+
+insertar_arista(grafo, 1, vertice1, vertice2)
+insertar_arista(grafo, 1, vertice1, vertice2)
+insertar_arista(grafo, 1, vertice1, vertice2)
+
+vertice3 = buscar_vertice(grafo, 3)
+vertice4 = buscar_vertice(grafo, 4)
+
+insertar_arista(grafo, 1, vertice1, vertice3)
+
+vertice5 = buscar_vertice(grafo, 5)
+
+mostrar_aristas(grafo)
--- huffman.py
+++ huffman.py
@@ -1,1 +1,69 @@
-#Huffman
+class NodoHuffman:
+    def __init__(self, caracter=None, frecuencia=0):
+        self.caracter = caracter
+        self.frecuencia = frecuencia
+        self.izquierda = None
+        self.derecha = None
+
+def construir_arbol_huffman(tabla):
+    nodos = [NodoHuffman(caracter, frecuencia) for caracter, frecuencia in tabla]
+    
+    while len(nodos) > 1:
+        nodos = sorted(nodos, key=lambda nodo: nodo.frecuencia)
+        nuevo_nodo = NodoHuffman()
+        nuevo_nodo.izquierda = nodos[0]
+        nuevo_nodo.derecha = nodos[1]
+        nuevo_nodo.frecuencia = nodos[0].frecuencia + nodos[1].frecuencia
+        nodos = nodos[2:]
+        nodos.append(nuevo_nodo)
+    
+    return nodos[0]
+
+def generar_tabla_codigos(raiz, codigo_actual, tabla_codigos):
+    if raiz.caracter:
+        tabla_codigos[raiz.caracter] = codigo_actual
+    else:
+        generar_tabla_codigos(raiz.izquierda, codigo_actual + "0", tabla_codigos)
+        generar_tabla_codigos(raiz.derecha, codigo_actual + "1", tabla_codigos)
+
+def decodificar_huffman(codigo, tabla_codigos):
+    codigo_actual = ""
+    texto_decodificado = ""
+    
+    for bit in codigo:
+        codigo_actual += bit
+        for caracter, codigo in tabla_codigos.items():
+            if codigo_actual == codigo:
+                texto_decodificado += caracter
+                codigo_actual = ""
+                break
+    
+    return texto_decodificado
+
+# Tabla de caracteres y códigos correspondientes
+tabla = [
+    ('I', 0.28),
+    ('N', 0.16),
+    ('T', 0.08),
+    ('E', 0.16),
+    ('L', 0.08),
+    ('G', 0.08),
+    ('C', 0.08),
+    ('A', 0.08)
+]
+
+# Construir el árbol de Huffman
+raiz_huffman = construir_arbol_huffman(tabla)
+
+# Generar la tabla de códigos
+tabla_codigos = {}
+generar_tabla_codigos(raiz_huffman, "", tabla_codigos)
+
+# Código a decodificar
+codigo = "10110010011001100000111101111101110"
+
+# Decodificar el código utilizando la tabla de códigos
+texto_decodificado = decodificar_huffman(codigo, tabla_codigos)
+
+# Imprimir el resultado
+print("Texto decodificado:", texto_decodificado)
